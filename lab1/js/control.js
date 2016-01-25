0fa18b99a5220c4a3da3c40aff9ceadb50803133
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2016-01-25 15:32:54 +0800

    More improvements on lab 1

--- lab1/js/control.js
+++ lab1/js/control.js
@@ -1,15 +1,28 @@
-function activateWorkbookOne() {
+function activateWorksheetOne() {
 	document.getElementById("section1").style.visibility = "visible";
 	document.getElementById("section1").style.display = "";
 	document.getElementById("section2").style.visibility = "hidden";
 	document.getElementById("section2").style.display = "none";
+	document.getElementById("section3").style.visibility = "hidden";
+	document.getElementById("section3").style.display = "none";
 }
 
-function activateWorkbookTwo() {
-    document.getElementById("section1").style.visibility = "hidden";
-    document.getElementById("section1").style.display = "none";
-    document.getElementById("section2").style.visibility = "visible";
+function activateWorksheetTwo() {
+	document.getElementById("section1").style.visibility = "hidden";
+	document.getElementById("section1").style.display = "none";
+	document.getElementById("section2").style.visibility = "visible";
 	document.getElementById("section2").style.display = "";
+	document.getElementById("section3").style.visibility = "hidden";
+	document.getElementById("section3").style.display = "none";
 }
 
-activateWorkbookOne();
+function activateWorksheetThree() {
+	document.getElementById("section1").style.visibility = "hidden";
+	document.getElementById("section1").style.display = "none";
+	document.getElementById("section2").style.visibility = "hidden";
+	document.getElementById("section2").style.display = "none";
+	document.getElementById("section3").style.visibility = "visible";
+	document.getElementById("section3").style.display = "";
+}
+
+activateWorksheetOne();
