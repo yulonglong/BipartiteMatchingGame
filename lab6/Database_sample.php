96f308a3b97ef4525ccc7311f1a158f739a1c9a9
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2016-03-11 02:15:54 +0800

    Added Database Time logging

--- lab6/Database_sample.php
+++ lab6/Database_sample.php
@@ -24,8 +24,16 @@ class Database {
 		$this->pdo = null;
 	}
 
+	public function getElapsedTime() {
+		return $this->queryTimeElapsed;
+	}
+
 	public function login($user_id, $password) {
-		$stmt = $this->pdo->query('SELECT * FROM user WHERE user_id = "'.$user_id.'"')->fetchAll();
+		$start = microtime(true);
+			$stmt = $this->pdo->query('SELECT * FROM user WHERE user_id = "'.$user_id.'"')->fetchAll();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
+
 		if (count($stmt) != 1) return -1;
 		if ($stmt[0]["hashed_password"] == crypt($password, $stmt[0]["hashed_password"])) {
 			return $stmt[0]["role"];
@@ -35,33 +43,53 @@ class Database {
 
 	public function submitScore($graphId, $username, $numMatch, $totalScore, $elapsed) {
 		$nowFormat = date('Y-m-d H:i:s');
-		$sql = "INSERT INTO score_table (graph_id,name,num_match,match_score,duration,date) VALUES (?,?,?,?,?,?)";
-		$this->pdo->prepare($sql)->execute([$graphId, $username, $numMatch, $totalScore, $elapsed, $nowFormat]);
+		$start = microtime(true);
+			$sql = "INSERT INTO score_table (graph_id,name,num_match,match_score,duration,date) VALUES (?,?,?,?,?,?)";
+			$this->pdo->prepare($sql)->execute([$graphId, $username, $numMatch, $totalScore, $elapsed, $nowFormat]);
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
 	}
 
 	public function getAllUsers() {
-		$stmt = $this->pdo->query('SELECT * FROM user ORDER BY user_id')->fetchAll();
+		$start = microtime(true);
+			$stmt = $this->pdo->query('SELECT * FROM user ORDER BY user_id')->fetchAll();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
 		return $stmt;
 	}
 
 	public function getBestScore($graphId) {
-		$stmt = $this->pdo->query('SELECT * FROM score_table WHERE graph_id = '.$graphId.' ORDER BY num_match DESC, match_score DESC, duration LIMIT 1')->fetchAll();
+		$start = microtime(true);
+			$stmt = $this->pdo->query('SELECT * FROM score_table WHERE graph_id = '.$graphId.' ORDER BY num_match DESC, match_score DESC, duration LIMIT 1')->fetchAll();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
+
 		if (count($stmt) == 0) return null;
 		return $stmt[0];
 	}
 
 	public function getTop10Score($graphId) {
-		$stmt = $this->pdo->query('SELECT * FROM score_table WHERE graph_id = '.$graphId.' ORDER BY num_match DESC, match_score DESC, duration LIMIT 10')->fetchAll();
+		$start = microtime(true);
+			$stmt = $this->pdo->query('SELECT * FROM score_table WHERE graph_id = '.$graphId.' ORDER BY num_match DESC, match_score DESC, duration LIMIT 10')->fetchAll();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
 		return $stmt;
 	}
 
 	public function resetHighScore($user_id) {
-		$stmt = $this->pdo->query('SELECT * FROM user WHERE user_id = "'.$user_id.'"')->fetchAll();
+		$start = microtime(true);
+			$stmt = $this->pdo->query('SELECT * FROM user WHERE user_id = "'.$user_id.'"')->fetchAll();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
+
 		if (count($stmt) != 1) return false;
 		if ($stmt[0]["role"] != 0) return false;
 
-		$sql = "TRUNCATE TABLE score_table";
-		$this->pdo->prepare($sql)->execute();
+		$start = microtime(true);
+			$sql = "TRUNCATE TABLE score_table";
+			$this->pdo->prepare($sql)->execute();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
 	}
 
 	public function registerUser($username, $email, $password, $confirm_password, &$feedback) {
@@ -74,15 +102,23 @@ class Database {
 			return false;
 		}
 
-		$stmt = $this->pdo->query('SELECT * FROM user WHERE user_id = "'.$username.'"')->fetchAll();
+		$start = microtime(true);
+			$stmt = $this->pdo->query('SELECT * FROM user WHERE user_id = "'.$username.'"')->fetchAll();
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
+
 		if (count($stmt) > 0) {
 			$feedback = "Username is already taken!";
 			return false;
 		}
 
-		$sql = "INSERT INTO user (user_id,hashed_password,role,email) VALUES (?,?,?,?)";
-		$this->pdo->prepare($sql)->execute([$username, crypt($password), 1, $email]);
-		$feedback = "Registration Successful!";
+		$start = microtime(true);
+			$sql = "INSERT INTO user (user_id,hashed_password,role,email) VALUES (?,?,?,?)";
+			$this->pdo->prepare($sql)->execute([$username, crypt($password), 1, $email]);
+			$feedback = "Registration Successful!";
+		$end = microtime(true);
+		$this->queryTimeElapsed += $end-$start;
+
 		return true;
 	}
 
