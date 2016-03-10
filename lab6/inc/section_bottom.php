5395979a3d8712f4fcaee2d7d401cb1e9afca926
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2016-03-10 13:42:29 +0800

    Moved highscore js to top

--- lab6/inc/section_bottom.php
+++ lab6/inc/section_bottom.php
@@ -1,4 +1,3 @@
 <script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
 <script src="js/helper.js"></script>
-<script src="js/globalControl.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
--- lab6/inc/section_head.php
+++ lab6/inc/section_head.php
@@ -1,6 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<script src="js/globalControl.js"></script>
+
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-7s5uDGW3AHqw6xtJmNNtr+OBRJUlgkNJEo78P4b0yRw= sha512-nNo+yCHEyn0smMxSswnf/OnX6/KwJuZTlNZBjauKhTK0c+zT+q5JOCx0UFhXQ6rJR9jg6Es8gPuD2uZcYDLqSw==" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto">
